2b95ec7d163d6b2b44d35eec823bd4f25b42f659
Strong214356 <tidiandelage1@gmail.com>
2025-11-13 20:13:04 +0100

    finished python piscine00

---
+++ 00/ex09/src/ft_package/__init__.py
@@ -0,0 +1,11 @@
+"""
+this is an example package
+"""
+
+from ft_package._package import count_in_list, ft_tqdm, makeAssertError
+
+__all__ = [
+    "count_in_list",
+    "ft_tqdm",
+    "makeAssertError"
+]
